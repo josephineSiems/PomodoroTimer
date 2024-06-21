56f470163eb4b435b4069413165bde14ec029c1f
finnsiems <josephine.siems@studium.uni-hamburg.de>
2024-06-21 17:56:45 +0200

    Fixed Bugs

--- app/src/main/java/com/example/pomodorotimer/FirstFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/FirstFragment.kt
@@ -1,7 +1,9 @@
 package com.example.pomodorotimer
 
 import android.content.Context
+import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.*
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +14,8 @@ import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.pomodorotimer.databinding.FragmentFirstBinding
+import kotlin.concurrent.timer
+import kotlin.math.round
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -27,6 +31,8 @@ class FirstFragment : Fragment() {
     //workTime and breakTime in minutes
     var workTime = 0
     var breakTime = 0
+    var alarmSound = ""
+    var volume = 5
 
     //workTime and breakTime in milliseconds
     var workMilli = 0L
@@ -35,10 +41,26 @@ class FirstFragment : Fragment() {
     var workMinutes = 0
     var breakMinutes = 0
 
+
+    var workSeconds = 0
+    var workSecs = 0
+    var breakSeconds = 0
+    var breakSecs = 0
+
     var secCounter = 60
 
     var alarmState = true
     var vibrationState = true
+    var first = true
+
+    lateinit var mediaPlayer: MediaPlayer
+    lateinit var timerWork: CountDownTimer
+    lateinit var timerBreak: CountDownTimer
+    lateinit var playMusicTime: CountDownTimer
+
+    var runningTimer = false
+    var timertype = "work"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,130 +77,91 @@ class FirstFragment : Fragment() {
 
         load()
         workMilli = workTime*60000L
+        workMilli += workSeconds*1000
         breakMilli = breakTime*60000L
+        breakMilli += breakSeconds*1000
+
+        workSecs = workSeconds +1
+        breakSecs = breakSeconds +1
 
         workMinutes = workTime
         breakMinutes = breakTime
 
-        setTimer(workMinutes, 0)
-
-
-        val resID = resources.getIdentifier("chime", "raw", activity?.packageName)
-        val mediaPlayer = MediaPlayer.create(view.context, resID)
-
-
-        try {
-            mediaPlayer.setLooping(true)
-        }catch (e:Exception){
-            Log.d("NoteThing","error: " + e)
-            }
-
-
-        val playMusicTime = object: CountDownTimer(2000, 1000){
-            override fun onTick(millisUntilFinished: Long) {
-            }
-            override fun onFinish() {
-                mediaPlayer.pause()
-            }
-        }
-
-
-        val timerBreak = object: CountDownTimer(breakMilli, 1000){
-            override fun onTick(millisUntilFinished: Long) {
-                if(breakMinutes == breakTime) {
-                    breakMinutes = breakMinutes - 1
-                }
-                secCounter = secCounter-1
-                if (secCounter < 1){
-                    secCounter = 59
-                    if (breakMinutes > 0) {
-                        breakMinutes = breakMinutes - 1
-                    }
-
-                }
-                setTimer(breakMinutes, secCounter)
-            }
-            override fun onFinish() {
-                Toast.makeText(context, "Time to Start Working!", Toast.LENGTH_LONG).show()
-                binding.buttonStart.performClick()
-                if (alarmState) {
-                    playJingle(mediaPlayer, playMusicTime)
-                }
-                if (vibrationState) {
-                    vibratePhone()
-                }
-                breakMinutes = breakTime
-                secCounter = 60
-            }
-        }
-
-        val timerWork = object: CountDownTimer(workMilli, 1000){
-            override fun onTick(millisUntilFinished: Long) {
-                if(workMinutes == workTime) {
-                    workMinutes = workMinutes - 1
-                }
-                secCounter = secCounter-1
-                if (secCounter < 1){
-                    secCounter = 59
-                    if(workMinutes > 0) {
-                        workMinutes = workMinutes - 1
-                    }
-
-                }
-                setTimer(workMinutes, secCounter)
-            }
-            override fun onFinish() {
-                Toast.makeText(context, "Time for a break :)", Toast.LENGTH_LONG).show()
-                timerBreak.start()
-                if (alarmState) {
-                    playJingle(mediaPlayer, playMusicTime)
-                }
-                if (vibrationState) {
-                    vibratePhone()
-                }
-                workMinutes = workTime
-                secCounter = 60
-            }
-        }
+        setTimerText(workMinutes, workSeconds)
+        setMediaPlayer()
+        setMusicTimer()
+        setBreakTimer()
+        setWorkTimer()
 
 
         binding.buttonStart.setOnClickListener {
-            timerWork.start()
-            Toast.makeText(this.context, "Time to Start Working!", Toast.LENGTH_LONG).show()
-        }
-
-        binding.buttonStop.setOnClickListener {
-            try {
-                timerWork.cancel()
-                timerBreak.cancel()
-                Toast.makeText(this.context, "Timer Stopped", Toast.LENGTH_LONG).show()
-            }catch (e:Exception){
-                Log.d("NoteThing", "No Timer: " + e)
-                Toast.makeText(this.context, "No Timer active", Toast.LENGTH_LONG).show()
+            if (!runningTimer && first) {
+                timerWork.start()
+                Toast.makeText(this.context, "Time to Start Working!", Toast.LENGTH_LONG).show()
+                binding.textviewTimeType.text = "Work Time"
+                binding.buttonStart.text = "Stop"
+                runningTimer = true
+                first = false
+            }else if(!runningTimer){
+                if(timertype == "work"){
+                    timerWork.start()
+                    runningTimer = true
+                }else{
+                    timerBreak.start()
+                    runningTimer = true
+                }
+                Toast.makeText(this.context, "Timer Resumed", Toast.LENGTH_LONG).show()
             }
-        }
-
-        try {
-            binding.buttonChange.setOnClickListener {
-                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            else {
+                try {
+                    timerWork.cancel()
+                    timerBreak.cancel()
+                    Toast.makeText(this.context, "Timer Stopped", Toast.LENGTH_LONG).show()
+                    binding.buttonStart.text = "Start"
+                    runningTimer = false
+                } catch (e: Exception) {
+                    Log.d("NoteThing", "No Timer: " + e)
+                    Toast.makeText(this.context, "No Timer active", Toast.LENGTH_LONG).show()
+                }
             }
-        }catch (e:Exception){
-            Log.d("NoteThing", "Exception: " + e)
         }
 
         binding.buttonRestart.setOnClickListener {
             try {
-                timerWork.cancel()
-                timerBreak.cancel()
+                if (runningTimer) {
+                    timerWork.cancel()
+                    timerBreak.cancel()
+                }
+
+                if (timertype == "work") {
+                    workMinutes = workTime
+                    workSecs = workSeconds + 1
+                    secCounter = 60
+                } else if (timertype == "break") {
+                    breakMinutes = breakTime
+                    breakSecs = breakSeconds + 1
+                    secCounter = 60
+                }
+
+
+                timerWork.start()
+                binding.textviewTimeType.text = "Work Time"
+                binding.buttonStart.text = "Stop"
+                runningTimer = true
+                Toast.makeText(this.context, "Timer Restarted", Toast.LENGTH_LONG).show()
+
             }catch (e:Exception){
                 Log.d("NoteThing", "No Timer: " + e)
             }
-            timerWork.start()
+
+        }
+
+        binding.buttonChange.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
-
-    fun setTimer(minute: Int, second: Int){
+    fun setTimerText(minute: Int, second: Int){
         var minZero = ""
         var secZero = ""
         if(minute < 10){
@@ -190,13 +173,160 @@ class FirstFragment : Fragment() {
         var time = minZero + minute.toString() + ":" + secZero + second.toString()
         binding.textviewTime.setText(time)
 
+        //secCounter = workSeconds
+
     }
+
+    fun setMediaPlayer(){
+        if (alarmSound != "") {
+            try {
+                var alarmPath = Uri.parse(alarmSound)
+                Log.d("NoteThing", "Song1: " + alarmPath)
+                mediaPlayer = MediaPlayer.create(context, alarmPath)
+
+            } catch (e: Exception){
+                Log.d("NoteThing", "Exception: " + e )
+                var alarmPath = resources.getIdentifier("chime", "raw", activity?.packageName)
+                mediaPlayer = MediaPlayer.create(context, alarmPath)
+            }
+        }else{
+            var alarmPath = resources.getIdentifier("chime", "raw", activity?.packageName)
+            mediaPlayer = MediaPlayer.create(context, alarmPath)
+            Log.d("NoteThing", "Song2: " + alarmPath)
+        }
+
+        try {
+            mediaPlayer.setLooping(true)
+            var volumePercent = ("0." + volume.toString()).toFloat()
+            mediaPlayer.setVolume(volumePercent,volumePercent)
+            Log.d("NoteThing", "Volume: " + volume)
+        }catch (e:Exception){
+            Log.d("NoteThing","error: " + e)
+        }
+    }
+
+    fun setMusicTimer() {
+        playMusicTime = object : CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                mediaPlayer.pause()
+            }
+        }
+    }
+
+    fun setBreakTimer(){
+        timerBreak = object: CountDownTimer(breakMilli, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("NoteThing", "break2")
+                if(breakMinutes == breakTime && breakSecs == 0) {
+                    breakMinutes--
+                }
+                if(breakSecs != 0){
+                    breakSecs--
+                    setTimerText(breakMinutes, breakSecs)
+                }
+
+                if (breakSecs == 0 && breakMinutes == 0){
+                    setTimerText(0, 0)
+                }
+                else if (breakSecs == 0){
+                    secCounter--
+                    Log.d("NoteThing", "secCounter1: " + secCounter)
+                    if (secCounter < 1){
+                        secCounter = 59
+                        if (breakMinutes > 0) {
+                            breakMinutes--
+                        }
+                    }
+                    setTimerText(breakMinutes, secCounter)
+                    Log.d("NoteThing", "secCounter please: " + secCounter)
+                }
+                Log.d("NoteThing", "secCounter2: " + secCounter + " breakSec: " + breakSecs)
+            }
+
+            override fun onFinish() {
+                binding.textviewTimeType.text = "Work Time"
+                Toast.makeText(context, "Time to Start Working!", Toast.LENGTH_LONG).show()
+                timertype = "work"
+
+                timerWork.start()
+                if (alarmState) {
+                    playJingle(mediaPlayer, playMusicTime)
+                }
+                if (vibrationState) {
+                    vibratePhone()
+                }
+                breakMinutes = breakTime
+                breakSecs = breakSeconds+1
+                secCounter = 60
+            }
+        }
+    }
+
+    fun setWorkTimer(){
+        timerWork = object: CountDownTimer(workMilli, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("NoteThing", "secCounter0: " + secCounter)
+                if(workMinutes == workTime && workSecs == 0) {
+                    workMinutes--
+                }
+
+                if(workSecs != 0){
+                    workSecs--
+                    setTimerText(workMinutes, workSecs)
+                }
+
+                if (workSecs == 0 && workMinutes == 0){
+                    setTimerText(0, 0)
+                }
+                else if(workSecs == 0 ){
+                    secCounter--
+                    Log.d("NoteThing", "secCounter1: " + secCounter)
+                    if (secCounter < 1){
+                        secCounter = 59
+                        if(workMinutes > 0) {
+                            workMinutes--
+                        }
+                    }
+                    setTimerText(workMinutes, secCounter)
+                    Log.d("NoteThing", "secCounter please: " + secCounter)
+                }
+                Log.d("NoteThing", "secCounter2: " + secCounter + " workSec: " + workSecs)
+
+            }
+            override fun onFinish() {
+                Log.d("NoteThing", "break")
+                binding.textviewTimeType.text = "Break Time"
+                Toast.makeText(context, "Time for a break :)", Toast.LENGTH_LONG).show()
+                timertype = "break"
+
+                timerBreak.start()
+                if (alarmState) {
+                    playJingle(mediaPlayer, playMusicTime)
+                }
+                if (vibrationState) {
+                    vibratePhone()
+                }
+                workMinutes = workTime
+                workSecs = workSeconds+1
+                secCounter = 60
+            }
+
+            fun restart() {
+                //
+
+            }
+        }
+    }
+
+
 
     fun playJingle(mediaPlayer:MediaPlayer, playMusic: CountDownTimer){
         //mediaPlayer.prepare()
         mediaPlayer.start()
         playMusic.start()
-        Log.d("NoteThing", "vibration: " + vibrationState + " alarm: " + alarmState)
     }
 
     fun vibratePhone() {
@@ -215,9 +345,14 @@ class FirstFragment : Fragment() {
         try {
             workTime = sharedPref.getInt("workTime", 0)
             breakTime = sharedPref.getInt("breakTime", 0)
+            workSeconds = sharedPref.getInt("workSeconds", 0)
+            breakSeconds = sharedPref.getInt("breakSeconds", 0)
             vibrationState = sharedPref.getBoolean("vibration", true)
             alarmState = sharedPref.getBoolean("alarm", true)
-            Log.d("NoteThing", "vibration: " + vibrationState + " alarm: " + alarmState)
+            alarmSound = sharedPref.getString("alarmsound", "").toString()
+            volume = sharedPref.getInt("volume", 5)
+
+            Log.d("NoteThing", "vibration: " + vibrationState + " alarm: " + alarmState + "alarmsound: " + alarmSound )
 
         }catch (e:Exception) {
             Log.d("NoteThing", ("No Counter:" + e))
--- app/src/main/java/com/example/pomodorotimer/SecondFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/SecondFragment.kt
@@ -1,15 +1,24 @@
 package com.example.pomodorotimer
 
+import android.R
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.CombinedVibration
+import android.provider.DocumentsContract
+import android.provider.OpenableColumns
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.pomodorotimer.databinding.FragmentSecondBinding
+import kotlin.math.round
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -22,10 +31,16 @@ class SecondFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     var vibration = false
-    var alarmsound = false
+    var alarm = false
     var workTime = 1
     var breakTime = 1
+    var workSeconds = 0
+    var breakSeconds = 0
+    var alarmsound = ""
+    var volume = 5
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,45 +61,88 @@ class SecondFragment : Fragment() {
         }
 
         binding.switchAlarmsound.setOnCheckedChangeListener { _, isChecked ->
-            alarmsound = isChecked
+            alarm = isChecked
         }
 
-        binding.picker1.maxValue = 100
-        binding.picker1.minValue = 1
-        binding.picker2.maxValue = 100
-        binding.picker2.minValue = 1
+        //work time
+        binding.picker1.maxValue = 60
+        binding.picker1.minValue = 0
+        binding.picker2.maxValue = 59
+        binding.picker2.minValue = 0
+
+        //break time
+        binding.picker3.maxValue = 60
+        binding.picker3.minValue = 0
+        binding.picker4.maxValue = 59
+        binding.picker4.minValue = 0
 
         try {
             binding.picker1.value = workTime
-            binding.picker2.value = breakTime
+            binding.picker2.value = workSeconds
+            binding.picker3.value = breakTime
+            binding.picker4.value = breakSeconds
+
 
             binding.switchVibration.isChecked = vibration
-            binding.switchAlarmsound.isChecked= alarmsound
+            binding.switchAlarmsound.isChecked= alarm
         }catch (e: Exception){
             Log.d("NoteThing", "Exception: " + e)
         }
 
+        binding.textviewVolumeNumber.text = volume.toString()
+
+        binding.buttonAlarmsoundUpload.setOnClickListener {
+            chooseAlarm()
+        }
+
+        binding.buttonVolumePlus.setOnClickListener {
+            if (volume >= 10){
+                volume = 10
+            }else{
+                volume ++
+            }
+            binding.textviewVolumeNumber.text = volume.toString()
+            Log.d("NoteThing", "Volume: " + volume)
+        }
+
+        binding.buttonVolumeMinus.setOnClickListener {
+            if (volume <= 1){
+                volume = 1
+            }else{
+                volume -= 1
+            }
+            binding.textviewVolumeNumber.text = volume.toString()
+            Log.d("NoteThing", "Volume: " + volume)
+        }
 
         binding.buttonSave.setOnClickListener {
             val pickerWork = binding.picker1.value
-            val pickerBreak = binding.picker2.value
+            val workSeconds = binding.picker2.value
 
-            Log.d("NoteThing", "vibration: " + vibration + " alarm: " + alarmsound)
+            val pickerBreak = binding.picker3.value
+            val breakSeconds = binding.picker4.value
 
-            save(pickerWork, pickerBreak, vibration, alarmsound)
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            Log.d("NoteThing", "vibration: " + vibration + " alarm: " + alarm)
+
+            save(pickerWork, pickerBreak, vibration, alarm, alarmsound, volume, workSeconds, breakSeconds)
+            //findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 
-    fun save(workTime: Int, breakTime: Int, vibration: Boolean, alarm: Boolean ){
+    fun save(workTime: Int, breakTime: Int, vibration: Boolean, alarm: Boolean, alarmsound: String, volume: Int, worksec: Int, breaksec: Int ){
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
         val editor = sharedPref.edit()
         try {
             editor.putInt("workTime", workTime)
             editor.putInt("breakTime", breakTime)
+            editor.putInt("workSeconds", worksec)
+            editor.putInt("breakSeconds", breaksec)
             editor.putBoolean("vibration", vibration)
             editor.putBoolean("alarm", alarm)
+            editor.putString("alarmsound", alarmsound)
+            editor.putInt("volume", volume)
             editor.apply()
+            Log.d("NoteThing", "vibration: " + vibration + " alarm: " + alarm + "alarmsound: " + alarmsound + " volume: " + volume )
         } catch (e: Exception) {
             Log.d("NoteThing", ("No Counter in Save:" + e))
         }
@@ -98,14 +156,39 @@ class SecondFragment : Fragment() {
         try {
             workTime = sharedPref.getInt("workTime", 1)
             breakTime = sharedPref.getInt("breakTime", 1)
+            workSeconds = sharedPref.getInt("workSeconds", 1)
+            breakSeconds = sharedPref.getInt("breakSeconds", 1)
             vibration = sharedPref.getBoolean("vibration", true)
-            alarmsound = sharedPref.getBoolean("alarm", true)
+            alarm = sharedPref.getBoolean("alarm", true)
+            alarmsound = sharedPref.getString("alarmsound", "").toString()
+            volume = sharedPref.getInt("volume", 5)
 
 
         }catch (e:Exception) {
             Log.d("NoteThing", ("No Counter:" + e))
         }
     }
+
+    fun chooseAlarm(){
+        val intent: Intent
+        intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "audio/mpeg"
+        startActivityForResult(intent,1)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            if (data != null && data.data != null) {
+                val audioFileUri = data.data
+                alarmsound = audioFileUri.toString()
+                Log.d("Pomo", audioFileUri.toString())
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
